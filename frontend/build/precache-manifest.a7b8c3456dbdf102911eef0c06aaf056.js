97c368c8cbcdf74b81c3830611f67f6b002733a9
vanditaa-sharma <78145034+vanditaa-sharma@users.noreply.github.com>
2023-02-11 20:16:21 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.a7b8c3456dbdf102911eef0c06aaf056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1050fc70a91ff3b2c3af4a898ef8a4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b857673038b7b646843",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b857673038b7b646843",
+    "url": "/static/js/main.1924460a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
